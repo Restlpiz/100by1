fea848f607beef8331441ba9635b871a7df4052e
Mepkypuu <mihail.kurguz2016@ynadex.ru>
2023-02-21 15:04:02 +0300

    Инициализация, верстка стартовой страницы Game.cshtml

---
+++ Game100by1/Controllers/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Game100by1.Controllers
+{
+    public class GameController : Controller
+    {
+        public ActionResult Game()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Information(string input, bool auth, string numberQuestion)
+        {
+            return Json("student saved successfully");
+        }
+    }
+}
---
+++ Game100by1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Game100by1.MvcApplication" Language="C#" %>
